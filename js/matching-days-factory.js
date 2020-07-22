5960c48a568b947fb379b4134fb41b3410ac0277
Ammaar <fakirammaar8@gmail.com>
2020-07-22 14:48:05 +0200

    ..

--- js/matching-days-factory.js
+++ js/matching-days-factory.js
@@ -1,41 +1,41 @@
 function MatchingDays() {
-    var week= ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-    function getDay(dateParam) {
-        var day1 = new Date(dateParam);
-        var day1Num = day1.getDay();
-        for(var i=0 ; i<week.length;i++){
-            var dayOfWeek=week[day1Num];
-            console.log(dayOfWeek)
-            return dayOfWeek;
+   var index;
+   var index2;
+    function getDay(day) {
+  index=day
 
-
-            }
         
-        return day1Num;
+     
     }
-    function getDay2(dateParam2) {
-
-        var day2 = new Date(dateParam2);
-
-        var day2Num = day2.getDay();
-        for(var i=0 ; i<week.length;i++){
-            var dayOfWeek=week[day2Num];
-            console.log(dayOfWeek);
-
-
-            }
-        return dayOfWeek;
+    function getDay2(day2) {
+index2=day2;
+      
     }
-  function dayStyles(){
-    const days=[{day:"Sunday",style:"mag"},{day:"Monday",style:"blue"},{day:"Tuesday",style:"yellow"},{day:"Wednesday",style:"orange"},{day:"Thursday",style:"red"},{day:"Friday",style:"coral"},{day:"Saturday",style:"blue"}]
-return days ;
+
+    function dayStyles(date,date2) {
+        const days = [{ day: "Sunday", style: "" }
+            , { day: "Monday", style: "" },
+        { day: "Tuesday", style: "" }
+            , { day: "Wednesday", style: "" }
+            , { day: "Thursday", style: "" }
+            , { day: "Friday", style: "" }
+            , { day: "Saturday", style: "" }]
+ if (date===date2) {
+days[].style=""
+ }     
+
+       
+       
+       
+       
+            return days;
 
 
-  }
+    }
     return {
         getDay,
         getDay2,
-dayStyles
+        dayStyles
     }
 
 }
