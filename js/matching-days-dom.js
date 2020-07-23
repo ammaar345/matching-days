366dfc300c48cd34c9e6331c73d7712d0fdb5844
Ammaar <fakirammaar8@gmail.com>
2020-07-23 22:06:34 +0200

    formatted code neatly

--- js/matching-days-dom.js
+++ js/matching-days-dom.js
@@ -1,27 +1,28 @@
 
- let matchdays=MatchingDays();
- var firstDate=document.querySelector(".date1");
- var secDate=document.querySelector(".date2");
+let matchdays = MatchingDays();
+var firstDate = document.querySelector(".date1");
+var secDate = document.querySelector(".date2");
 
- var tempObjStart=matchdays.dayStyles()
- result.innerHTML=matchTemplate( { days : tempObjStart });
-function change(){
-   result.innerHTML=""
-   var firstVal=firstDate.value;
-   var secVal=secDate.value;
-   var date=new Date(firstVal);
-   var date2=new Date(secVal);
-   var dayVal1=date.getDay();
-   var dayVal2=date2.getDay()
-   if(firstVal){
-   matchdays.setDayOfWeek(dayVal1)
-   var tempObj=matchdays.dayStyles()}
-   if (secVal ){
-   matchdays.setDayOfWeek2(dayVal2)
-   var tempObj=matchdays.dayStyles()
-}
-result.innerHTML=matchTemplate( { days : tempObj });
+var tempObjStart = matchdays.dayStyles()
+result.innerHTML = matchTemplate({ days: tempObjStart });
+function change() {
+   result.innerHTML = ""
+   var firstVal = firstDate.value;
+   var secVal = secDate.value;
+   var date = new Date(firstVal);
+   var date2 = new Date(secVal);
+   var dayVal1 = date.getDay();
+   var dayVal2 = date2.getDay()
+   if (firstVal) {
+      matchdays.setDayOfWeek(dayVal1)
+      var tempObj = matchdays.dayStyles()
+   }
+   if (secVal) {
+      matchdays.setDayOfWeek2(dayVal2)
+      var tempObj = matchdays.dayStyles()
+   }
+   result.innerHTML = matchTemplate({ days: tempObj });
 
 }
-    firstDate.addEventListener("change",change)
- secDate.addEventListener("change",change)
+firstDate.addEventListener("change", change)
+secDate.addEventListener("change", change)
--- js/matching-days-factory.js
+++ js/matching-days-factory.js
@@ -1,18 +1,18 @@
 function MatchingDays() {
-   var dayindex;
-   var dayindex2;
+    var dayindex;
+    var dayindex2;
     function setDayOfWeek(day) {
-  dayindex=day
-        
+        dayindex = day
+
     }
     function setDayOfWeek2(day2) {
-        
-dayindex2=day2;
+
+        dayindex2 = day2;
 
     }
 
     function dayStyles() {
-      
+
         const days = [{ day: "Sunday", style: "" }
             , { day: "Monday", style: "" },
         { day: "Tuesday", style: "" }
@@ -20,32 +20,29 @@ dayindex2=day2;
             , { day: "Thursday", style: "" }
             , { day: "Friday", style: "" }
             , { day: "Saturday", style: "" }]
- if ((dayindex===0)|| (dayindex2===0)){
-if(dayindex!==dayindex2)
-{
-    days[dayindex].style="red";
-days[dayindex2].style="yellow"
-}
-else if(dayindex===dayindex2)
-{
-    days[dayindex].style="green";
-}
- }
-  if(dayindex && dayindex2 && dayindex!==dayindex2)
- {
-days[dayindex].style="red";
-days[dayindex2].style="yellow"
+        if ((dayindex === 0) || (dayindex2 === 0)) {
+            if (dayindex !== dayindex2) {
+                days[dayindex].style = "red";
+                days[dayindex2].style = "yellow"
+            }
+            else if (dayindex === dayindex2) {
+                days[dayindex].style = "green";
+            }
+        }
+        if (dayindex && dayindex2 && dayindex !== dayindex2) {
+            days[dayindex].style = "red";
+            days[dayindex2].style = "yellow"
 
- } else if (dayindex && dayindex2 && dayindex===dayindex2) {
-    days[dayindex].style="green";
-   
-    }   
+        } else if (dayindex && dayindex2 && dayindex === dayindex2) {
+            days[dayindex].style = "green";
 
-       
-       
-       
-       
-            return days;
+        }
+
+
+
+
+
+        return days;
 
 
     }
