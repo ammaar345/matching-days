d5fa0b954f3bf1131e135dd582fc9d02ddcec7c9
Ammaar <fakirammaar8@gmail.com>
2020-07-25 08:29:14 +0200

    changed how days style

--- js/matching-days-dom.js
+++ js/matching-days-dom.js
@@ -3,33 +3,32 @@ let matchdays = MatchingDays();
 var firstDate = document.querySelector(".date1");
 var secDate = document.querySelector(".date2");
 
-var tempobj=[{ day: "Sunday", style: "" }
-, { day: "Monday", style: "" },
+var tempobj = [{ day: "Sunday", style: "" }
+   , { day: "Monday", style: "" },
 { day: "Tuesday", style: "" }
-, { day: "Wednesday", style: "" }
-, { day: "Thursday", style: "" }
-, { day: "Friday", style: "" }
-, { day: "Saturday", style: "" }]
+   , { day: "Wednesday", style: "" }
+   , { day: "Thursday", style: "" }
+   , { day: "Friday", style: "" }
+   , { day: "Saturday", style: "" }]
 
 
-result.innerHTML = matchTemplate({ days: tempobj});
+result.innerHTML = matchTemplate({ days: tempobj });
 function change() {
-
+   result.innerHTML=""
    var firstVal = firstDate.value;
    var secVal = secDate.value;
-   var date = new Date(firstVal);
-   var date2 = new Date(secVal);
-   
-   var dayVal1 = date.getDay();
-   var dayVal2 = date2.getDay()
-   matchdays.setDayOfWeek(dayVal1)
-
-   matchdays.setDayOfWeek2(dayVal2)
-   // matchdays.dayStyles()
-
-
+   if (firstVal) {
+      var date = new Date(firstVal);
+      var dayVal1 = date.getDay();
+      matchdays.setDayOfWeek(dayVal1)
+   }
+   ///else 
+   if (secVal) {
+      var date2 = new Date(secVal);
+      var dayVal2 = date2.getDay()
+      matchdays.setDayOfWeek2(dayVal2)
+   }
    result.innerHTML = matchTemplate({ days: matchdays.dayStyles() });
-
 }
 firstDate.addEventListener("change", change)
 secDate.addEventListener("change", change)
